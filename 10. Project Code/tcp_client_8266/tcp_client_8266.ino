be8b1ed6c81850c336bf3885df70e11085ff7353
heroydx <312220241@qq.com>
2018-01-26 14:03:47 +0800

    with static IP for both server & client

--- 10. Project Code/tcp_client_8266/tcp_client_8266.ino
+++ 10. Project Code/tcp_client_8266/tcp_client_8266.ino
@@ -1,5 +1,7 @@
 /*在NodeMCU搭建了TCP client端，进行简单通信*/
 /*参考代码：https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/examples/WiFiClient/WiFiClient.ino */
+/* 连接路由器并设置静态IP */
+/* 参考代码：https://github.com/esp8266/Arduino/issues/1959 */
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -8,7 +10,7 @@
 const char* ssid = "BUPT_J2_1776"; //填入自己的WiFi名
 const char* password = "bupt12345678"; //WiFi密码
 
-const char* host = "192.168.2.120";//server ip
+const char* host = "192.168.1.200";//server ip
 
 // Start a TCP Server on port 5045
 WiFiServer server(5045); //端口5045，自定义（避免公用端口）
@@ -18,44 +20,54 @@ char data[1500];
 int ind = 0;
 
 void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid,password);
+  // put your setup code here, to run once:
+  Serial.begin ( 115200 );
+  WiFi.begin(ssid, password);
   Serial.println("");
   //Wait for connection
-  while(WiFi.status() != WL_CONNECTED) { //检查WiFi连接状态
+  while (WiFi.status() != WL_CONNECTED) {
+    //检查WiFi连接状态
     delay(500);
     Serial.print(".");
   }
+
+  // config static IP
+  IPAddress ip(192, 168, 1, 201); // where xx is the desired IP Address
+  IPAddress gateway(192, 168, 0, 1); // set gateway to match your network
+  Serial.print(F("Setting static ip to : "));
+  Serial.println(ip);
+  IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your network
+  WiFi.config(ip, gateway, subnet);
+
   Serial.print("Connected to "); Serial.println(ssid);
   Serial.print("IP Address: "); Serial.println(WiFi.localIP()); //串口监视器显示IP地址
 
   // Start the TCP server
   server.begin();
-}
 
-//int value = 0;
+}
 
 void loop() {
   delay(5000);
   //++value;
 
   //server = 192.168.2.119; //以后改为自动获取对方ip
-  Serial.print(WiFi.localIP());Serial.println("connecting to host.....");
+  Serial.print(WiFi.localIP()); Serial.println("connecting to host.....");
   //Serial.println(host);
-  
+
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   //const int httpPort = 80;
   const int Port = 5045;
-//  if (!client.connect(host, httpPort)) {
-//    Serial.println("connection failed");
-//    return;
-//  }
+  //  if (!client.connect(host, httpPort)) {
+  //    Serial.println("connection failed");
+  //    return;
+  //  }
   if (!client.connect(host, Port)) {
     Serial.println("connection failed");
     return;
   }
-  
+
   // We now create a URI for the request
   String url = "hello";
   //url += streamId;
@@ -63,13 +75,13 @@ void loop() {
   //url += privateKey;
   //url += "&value=";
   //url += value;
-  
+
   Serial.print("Requesting: ");
   Serial.println(url);
-  
+
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
+               "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
   unsigned long timeout = millis();
   while (client.available() == 0) {
@@ -79,13 +91,13 @@ void loop() {
       return;
     }
   }
-  
+
   // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
+  while (client.available()) {
     String line = client.readStringUntil('\r');
     Serial.print(line);
   }
-  
+
   Serial.println();
   Serial.println("closing connection");
 }
--- 10. Project Code/tcp_server_8266/tcp_server_8266.ino
+++ 10. Project Code/tcp_server_8266/tcp_server_8266.ino
@@ -1,5 +1,7 @@
 /*在NodeMCU搭建了TCP server端，PC为client端与server进行简单通信*/
 /*参考代码：https://zhuanlan.zhihu.com/p/27668432 */
+/* 连接路由器并设置静态IP */
+/* 参考代码：https://github.com/esp8266/Arduino/issues/1959 */
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -16,44 +18,56 @@ char data[1500];
 int ind = 0;
 
 void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid,password);
+  // put your setup code here, to run once:
+  Serial.begin ( 115200 );
+  WiFi.begin(ssid, password);
   Serial.println("");
   //Wait for connection
-  while(WiFi.status() != WL_CONNECTED) { //检查WiFi连接状态
+  while (WiFi.status() != WL_CONNECTED) {
+    //检查WiFi连接状态
     delay(500);
     Serial.print(".");
   }
+
+  // config static IP
+  IPAddress ip(192, 168, 1, 200); // where xx is the desired IP Address
+  IPAddress gateway(192, 168, 0, 1); // set gateway to match your network
+  Serial.print(F("Setting static ip to : "));
+  Serial.println(ip);
+  IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your network
+  WiFi.config(ip, gateway, subnet);
+
   Serial.print("Connected to "); Serial.println(ssid);
   Serial.print("IP Address: "); Serial.println(WiFi.localIP()); //串口监视器显示IP地址
 
   // Start the TCP server
   server.begin();
+
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    if(!client.connected())
+  // put your main code here, to run repeatedly:
+  if (!client.connected())
   {
     //try to connect to a new client
-    Serial.print(WiFi.localIP());Serial.println(" try again....");
+    Serial.print(WiFi.localIP()); Serial.println(" try again....");
     client = server.available();
   }
   else
   {
-    if(client.available() > 0)
+    if (client.available() > 0)
     {
       //Serial.println("Connected to client");
-      Serial.print(WiFi.localIP());Serial.println(" listenning....");
+      Serial.print(WiFi.localIP()); Serial.println(" listenning....");
 
-      while(client.available())
+      while (client.available())
       {
         data[ind] = client.read(); //读取client端发送的字符
         ind++;
       }
       client.flush();
 
-      for(int j=0;j < ind; j++)
+      for (int j = 0; j < ind; j++)
       {
         Serial.print(data[j]);
       }
@@ -64,4 +78,5 @@ void loop() {
     }
 
   }
+
 }
